6f648864d3e5eca7fcfae2d72f8ed1c34bfd61df
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-04-07 12:21:13 +0300

    fix: fix bug with backspace and arrowLeft, change styles

--- src/js/utils/inputUtils.js
+++ src/js/utils/inputUtils.js
@@ -27,15 +27,22 @@ function handlePressBackspace({ textarea }) {
   const input = textarea;
   const { value, selectionStart, selectionEnd } = input;
 
-  if (selectionStart !== selectionEnd) {
+  if ((selectionStart !== selectionEnd) && (selectionStart !== 0)) {
     input.value = value.slice(0, selectionStart) + value.slice(selectionEnd);
     input.selectionStart = selectionStart;
-  } else {
+    input.selectionEnd = selectionStart;
+  } else if ((selectionStart !== selectionEnd) && (selectionStart === 0)) {
+    input.value = value.slice(selectionEnd);
+    input.selectionStart = 0;
+    input.selectionEnd = 0;
+  } else if ((selectionStart === selectionEnd) && (selectionStart !== 0)) {
     input.value = value.slice(0, selectionStart - 1) + value.slice(selectionStart);
     input.selectionStart = selectionStart - 1;
+    input.selectionEnd = selectionStart - 1;
+  } else {
+    input.selectionStart = 0;
+    input.selectionEnd = 0;
   }
-
-  input.selectionEnd = selectionStart - 1;
 }
 
 function handlePressDelete({ textarea }) {
@@ -60,16 +67,25 @@ function handlePressArrowLeft({ textarea, isShift }) {
     if (selectionStart !== selectionEnd) {
       input.selectionStart = selectionStart;
       input.selectionEnd = selectionStart;
+    } else if (selectionStart === 0) {
+      input.selectionStart = 0;
+      input.selectionEnd = 0;
     } else {
       input.selectionStart = selectionStart - 1;
       input.selectionEnd = selectionStart - 1;
     }
-  } else if (selectionDirection === 'forward') {
-    // console.log('ArrowLeft --- forward', selectionStart, selectionEnd, selectionDirection);
+  } else if ((selectionDirection === 'forward') && (selectionStart !== 0)) { // --------------------------------
+    input.selectionStart = selectionStart;
     input.selectionEnd = selectionEnd - 1;
-  } else if (selectionDirection === 'backward') {
-    // console.log('ArrowLeft --- backward', selectionStart, selectionEnd, selectionDirection);
+  } else if ((selectionDirection === 'forward') && (selectionStart === 0)) { // --------------------------------
+    input.selectionStart = 0;
+    input.selectionEnd = selectionEnd - 1;
+  } else if ((selectionDirection === 'backward') && (selectionStart !== 0)) {
     input.selectionStart = selectionStart - 1;
+    input.selectionEnd = selectionEnd;
+  } else {
+    input.selectionStart = 0;
+    input.selectionEnd = selectionEnd;
   }
 }
 
